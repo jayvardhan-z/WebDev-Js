88fd6d44bec2d4e499d268da1138bc3fb5392c49
Jayvardhan <jayvardhanthorat07@gmail.com>
2026-03-15 08:44:22 +0000

    summarised DataTypes must read

---
+++ 01_basics/05-summaryOfDatatypes.js
@@ -0,0 +1,55 @@
+// JavaScript is a dynamically typed language. This means that variable types are determined at runtime
+
+//  Primitive
+
+//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log(id === anotherId);
+
+const bigNumber = 3456543576654356754n
+
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof myFunction);
+
+// Return type of variables in JavaScript
+// =======================
+//  Primitive Datatypes
+// ---------------------------------------------------
+//        Number =>     number
+//        String  =>        string
+//        Boolean  =>    boolean
+//        null  =>             object
+//        undefined  =>  undefined
+//        Symbol  =>      symbol
+//        BigInt  =>         bigint
+// ========================
+//  Non-primitive Datatypes
+// ---------------------------------------------
+//        Arrays  =>       object
+//        Function  =>  function
+//        Object  =>       object
